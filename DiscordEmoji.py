2d352be29af9ae35106464a8f1e508fbc7afb5d7
jacksonb2021 <jacksonburns2021@gmail.com>
2024-03-25 12:50:51 -0700

    discord emoji update
    Took 58 seconds

--- DiscordEmoji.py
+++ DiscordEmoji.py
@@ -5,19 +5,16 @@ str = input("Enter text to discordify\n")
 output = ""
 while str!="e":
     for i in str:
-        if i==' ':
-            output+="  "
-        elif i=='b':
-            output+=":b: "
-        else:
-            output+=":regional_indicator_"+i+": "
+        if i.isalpha():
+            if i==' ':
+                output+=":black_large_square: "
+            elif i=='b':
+                output+=":b: "
+            else:
+                output+=":regional_indicator_"+i+": "
     clipboard.OpenClipboard()
     clipboard.EmptyClipboard()
     clipboard.SetClipboardText(output)
     clipboard.CloseClipboard()
     str = input("Enter text to discordify\n")
     output=""
-
-
-
-
